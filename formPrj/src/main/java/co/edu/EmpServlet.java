d2d441f1879fa698ca68a900afff6aa63ee8e164
han <siny9713@gmail.com>
2022-06-24 09:27:43 +0900

    0623 어제안올라간부분

---
+++ formPrj/src/main/java/co/edu/EmpServlet.java
@@ -0,0 +1,35 @@
+package co.edu;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@WebServlet({ "/EmpServlet", "/Emplist" })
+public class EmpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public EmpServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		EmpDAO dao = new EmpDAO();
+		List<Employee>list = dao.empList();
+		Gson gson = new GsonBuilder().create();
+		response.getWriter().print(gson.toJson(list)); // 응답받은 결과를 화면에 출력 
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
